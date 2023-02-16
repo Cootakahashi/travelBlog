87b7ae98ddbe9497e0c49706618ba082956b4cf0
Cootakahashi <saiko.life2020@gmail.com>
2023-02-16 19:31:21 +0900

    added className NavBar.js

--- components/NavBar.js
+++ components/NavBar.js
@@ -6,16 +6,18 @@ function NavBar() {
       <div className="flex p-2">
         <Image src="logo.svg" alt="logo" width={129} height={61} />
       </div>
-      <div className="flex">
+      <div className="flex gap-16">
         <div className="p-2 cursor-pointer">ABOUT US</div>
         <div className="p-2 cursor-pointer">EXPERIENCES</div>
+        <div className="p-2 cursor-pointer">BLOGS</div>
+
         <div className="p-2 cursor-pointer">FAMILY TRAVEL</div>
         <div className="p-2 mr-2 cursor-pointer">TRAVEL TIPS</div>
-        <div class="my-auto ml-auto block cursor-pointer pr-4">
+        <div className="my-auto ml-auto block cursor-pointer pr-4">
           <div id="burger">
-            <div class="relative top-0 h-1 w-8 rounded-r bg-stone-700 shado"></div>
-            <div class="mt-1 h-1 w-8 rounded-r bg-stone-700 opacity-100"></div>
-            <div class="relative top-0 mt-1 h-1 w-8 rounded-r bg-stone-700"></div>
+            <div className="relative top-0 h-1 w-8 rounded-r bg-stone-700 shado"></div>
+            <div className="mt-1 h-1 w-8 rounded-r bg-stone-700 opacity-100"></div>
+            <div className="relative top-0 mt-1 h-1 w-8 rounded-r bg-stone-700"></div>
           </div>
         </div>
       </div>
