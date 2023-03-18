7eec1480693d93ede74b82c892afc2325459d326
earielle <demix3991@gmail.com>
2023-03-18 17:57:48 +0700

    Swiper responsive

--- components/SwiperApp.js
+++ components/SwiperApp.js
@@ -1,42 +1,49 @@
-import React, { useRef, useState } from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import Image from "next/image";
 import Link from "next/link";
 
 import "swiper/css";
-// import "swiper/css/pagination";
+import "swiper/css/pagination";
+import "swiper/css/scrollbar";
 
-import { Pagination } from "swiper";
+import { Pagination, FreeMode, Scrollbar } from "swiper";
 
 export default function SwiperApp({ blog }) {
   return (
     <>
       <div className="mr-10">
         <Swiper
-          slidesPerView={3}
-          spaceBetween={170}
-          slidesOffsetAfter={50}
-          slidesOffsetBefore={50}
+          slidesPerView={1}
+          spaceBetween={0}
+          // slidesOffsetAfter={50}
+          // slidesOffsetBefore={150}
           allowSlideNext={true}
           autoHeight={false}
-          freeMode={true}
+          freeMode
+          centeredSlides
+          grabCursor
+          centeredSlidesBounds
           pagination={{
             clickable: true,
           }}
           breakpoints={{
-            320: { slidesPerView: 1, spaceBetween: 10 },
-            532: { slidesPerView: 2, spaceBetween: 30 },
-            791: { slidesPerView: 3, spaceBetween: 50 },
-            1024: { slidesPerView: 3, spaceBetween: 90 },
+            250: { slidesPerView: 1, slidesOffsetBefore: 42 },
+            432: { slidesPerView: 1.5, slidesOffsetBefore: 42 },
+            755: { slidesPerView: 2, slidesOffsetBefore: 42 },
+            1024: {
+              slidesPerView: 3,
+              spaceBetween: 90,
+              slidesOffsetBefore: 42,
+            },
           }}
-          // modules={[Pagination]}
-          className="mySwiper max-w-screen-2xl"
+          modules={[Pagination, FreeMode, Scrollbar]}
+          className="max-w-screen-2xl"
         >
-          {blog.map((e) => {
+          {blog.map((e, id) => {
             const imageExist = e.eyecatch?.url;
             if (imageExist) {
               return (
-                <SwiperSlide>
+                <SwiperSlide key={e.id}>
                   <div className="min-w-[300px] max-w-[345px] shadow-4xl rounded-3xl relative">
                     <Image
                       className="border-[#FFC9AA] border-[3px] rounded-t-md  h-[360px] min-w-0"
@@ -52,7 +59,7 @@ export default function SwiperApp({ blog }) {
                     </p>
                     <div className="h-16 bg-zinc-100 rounded-b-xl flex justify-end shadow-[10px-10px-10px-10px]">
                       <Link
-                        className="hover:text-blue-300 hover:bg-slate-300 transition transition duration-500 ease-in-out bg-zinc-500 self-center text-zinc-50 rounded-md px-6 py-1 font-bold mr-6 mb-2"
+                        className="hover:text-blue-300 hover:bg-slate-300 transition duration-500 ease-in-out bg-zinc-500 self-center text-zinc-50 rounded-md px-6 py-1 font-bold mr-6 mb-2"
                         href={`/blog/${e.id}`}
                       >
                         Read more
